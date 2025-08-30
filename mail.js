efd99459c805c3ab1702e589f911996f3e1ed605
Saurav kumar <sauravgohill45@gmail.com>
2025-08-30 21:56:32 +0530

    v1.0

---
+++ mail.js
@@ -0,0 +1,18 @@
+function sendEmail(event) {
+    if (event) event.preventDefault();
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+    emailjs.send("service_7y1kqq9", "template_oxvtr4h", params)
+        .then(function(response) {
+         
+            alert("Message sent successfully!");
+        })
+        .catch(function(error) {
+            console.error("Error sending email:", error);
+            alert("Failed to send message. Please try again later.");
+        });
+}
