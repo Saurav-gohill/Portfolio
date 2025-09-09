4e9141d0e0ddd74e657b86ed7dc49b8e21b59c5c
Saurav kumar <sauravgohill45@gmail.com>
2025-09-10 01:04:46 +0530

    update1.9

--- mail.js
+++ mail.js
@@ -1,14 +1,21 @@
 function sendEmail(event) {
     if (event) event.preventDefault();
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email1").value.trim();
+    let subject = document.getElementById("subject").value.trim();
+    let message = document.getElementById("message").value.trim();
+    if (!name || !email || !subject || !message) {
+        alert("Contact section detail can't be empty to send message.");
+        return;
+    }
     let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email1").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
     };
     emailjs.send("service_7y1kqq9", "template_oxvtr4h", params)
         .then(function(response) {
-         
             alert("Message sent successfully!");
         })
         .catch(function(error) {
